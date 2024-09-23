0d372933865f9e5c305bd4c70ede0242ce1acbad
Gabdykalik <adletgabdykalik@gmail.com>
2024-09-23 19:54:13 +0500

    edited nuxt config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,11 +4,4 @@ export default defineNuxtConfig({
   modules: ["@nuxtjs/tailwindcss", "@vueuse/nuxt"],
   ssr: false,
   compatibilityDate: "2024-08-26",
-  target: "static",
-  app: {
-    baseURL: "https://github.com/Gabdykalik/trello-mock",
-  },
-  generate: {
-    fallback: true,
-  },
 });
