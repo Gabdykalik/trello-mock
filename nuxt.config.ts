14d4e8f1ce9d0e7423f771c9556741e10479ea89
Gabdykalik <adletgabdykalik@gmail.com>
2024-09-23 21:08:08 +0500

    gh pages deploy

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,4 +4,11 @@ export default defineNuxtConfig({
   modules: ["@nuxtjs/tailwindcss", "@vueuse/nuxt"],
   ssr: false,
   compatibilityDate: "2024-08-26",
+  target: "static",
+  app: {
+    baseURL: "https://gabdykalik.github.io/trello-mock/", // Set to the name of your GitHub repo
+  },
+  generate: {
+    fallback: "404.html", // Ensure proper handling of 404 errors
+  },
 });
