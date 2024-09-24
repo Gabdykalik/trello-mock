aa0209fc49b577567cca7ad797c3fee0d529c1d7
Gabdykalik <adletgabdykalik@gmail.com>
2024-09-24 21:22:25 +0500

    deployment fix

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,14 +1,9 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
   modules: ["@nuxtjs/tailwindcss", "@vueuse/nuxt"],
   ssr: false,
   compatibilityDate: "2024-08-26",
-  target: "static",
   app: {
-    baseURL: "https://gabdykalik.github.io/trello-mock/", // Set to the name of your GitHub repo
-  },
-  generate: {
-    fallback: "404.html", // Ensure proper handling of 404 errors
+    baseURL: "/trello-mock/",
   },
 });
