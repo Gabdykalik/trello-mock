89750fdf9b612bcca50faec78490c33f5de88b8f
Gabdykalik <adletgabdykalik@gmail.com>
2024-10-20 19:56:08 +0500

    deployment fix

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,8 +3,10 @@ export default defineNuxtConfig({
   compatibilityDate: "2024-04-03",
   devtools: { enabled: true },
   modules: ["@nuxtjs/tailwindcss", "@vueuse/nuxt"],
-  target: "static",
-  router: {
-    base: "/trello-mock/",
+  app: {
+    baseURL: process.env.NODE_ENV === "production" ? "/nuxt3-gh-pages/" : "/", // repo name
+  },
+  build: {
+    transpile: ["vuedraggable"], // Ensure vuedraggable works properly in build
   },
 });
